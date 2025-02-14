44836e8a78ea3f9b8a261f41b48cd3b64dad4dc7
DESKTOP-KQUB8HD\ghjang <jmxexpert@hotmail.com>
2025-02-14 21:24:52 +0900

    주석 내용 업데이트

--- _concept_summary/chapter 2/dao_construction_xml_config/src/main/java/com/example/daofactory/ArticleController.java
+++ _concept_summary/chapter 2/dao_construction_xml_config/src/main/java/com/example/daofactory/ArticleController.java
@@ -12,11 +12,7 @@ public class ArticleController {
      * NOTE:
      * 'applicationContext.xml'에서 'PropertyPlaceholderConfigurer'를
      * 사용하여 '스프링 컨테이너' 컨텍스트에 로딩한 'application.properties' 파일의
-     * 'dao.type' 프로퍼티 값을 'daoType' 멤버 변수에 주입한다.
-     *
-     * 'AutowiredAnnotationBeanPostProcessor' 빈을 컨텍스트에 등록했기 때문에
-     * 이렇게 '임의 .java' 파일에서 '@Value' 어노테이션을 사용하여
-     * 'application.properties' 파일의 프로퍼티 값을 주입받을 수 있다.
+     * 'dao.type' 프로퍼티 값을 'daoType' 멤버 변수에 주입할 수 있습니다.
      */
     @Value("${dao.type}")
     private String daoType;
